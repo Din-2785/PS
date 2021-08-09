da902a2002c6731f8d8ca220a3d554e29264e642
Din-2785 <bjn112@naver.com>
2021-08-09 23:16:42 +0900

    Create A - Way Too Long Words.cpp

---
+++ CodeForces/A - Way Too Long Words.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[ 101 ] ;
+
+	size_t n = 0 ;
+	scanf( "%d", &n ) ;
+
+	for( size_t i = 0; i < n; i++ )
+	{
+		scanf( "%s", str ) ;
+
+		size_t numOfStr = strlen( str ) ;
+
+		if( 10 < numOfStr )
+		{
+			printf( "%c%d%c\n", str[ 0 ], numOfStr - 2, str[ numOfStr - 1 ] ) ;
+		}
+		else
+		{
+			printf( "%s\n", str ) ;
+		}
+	}
+}
