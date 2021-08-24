ae44aeeee39f521cecb68957883fb088eb318104
Din-2785 <bjn112@naver.com>
2021-08-24 22:48:33 +0900

    2021-08-24 PS

---
+++ CodeForces/158A - Next Round.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+
+int main()
+{
+	size_t n = 0 ;
+	size_t k = 0 ;
+	size_t list_k[ 51 ] = { 0, } ;
+
+	std::cin >> n >> k ;
+
+	for( size_t i = 1; i <= n; i++ )
+	{
+		std::cin >> list_k[ i ] ;
+	}
+	
+	size_t biggerCnt = 0 ;
+
+	for( size_t i = 1; i <= n; i++ )
+	{
+		if( 0 < list_k[ i ] )
+		{
+			if( list_k[ i ] >= list_k[ k ] )
+			{
+				biggerCnt++ ;
+			}
+		}
+	}
+
+	std::cout << biggerCnt ;
+}
