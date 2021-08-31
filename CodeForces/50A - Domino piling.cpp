31b64d13daaf5605a1b6b0623dbebcb046ce944c
Din-2785 <bjn112@naver.com>
2021-08-31 16:26:44 +0900

    Create 50A - Domino piling.cpp

---
+++ CodeForces/50A - Domino piling.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+
+int main()
+{
+	size_t m, n ;
+	
+	std::cin >> m >> n ;
+
+	size_t outPut = static_cast< size_t >( ( m * n ) / 2 ) ;
+
+	std::cout << outPut ;
+
+	return 0 ;
+}
