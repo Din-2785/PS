5f8d2148387d2bad83b914334e188072a03edd1e
Din <bjn112@naver.com>
2022-01-19 23:21:07 +0900

    Update 9095.cpp

--- BOJ/9095.cpp
+++ BOJ/9095.cpp
@@ -4,12 +4,11 @@ int main()
 {
 	int d[ 12 ] = { 0, } ;
 
-	d[ 0 ] = 0 ;
+	d[ 0 ] = 1 ;
 	d[ 1 ] = 1 ;
 	d[ 2 ] = 2 ;
-	d[ 3 ] = 4 ;
 
-	for ( size_t i = 4; i <= 11; i++ )
+	for ( size_t i = 3; i <= 11; i++ )
 	{
 		d[ i ] = d[ i - 1 ] + d[ i - 2 ] + d[ i - 3 ] ;
 	}
@@ -21,7 +20,7 @@ int main()
 	{
 		int n = 0 ;
 		std::cin >> n ;
-		
+
 		std::cout << d[ n ] << '\n' ;
 	}
 
