c1f809894a7210de916829201a8d05de01ce22ed
Din <bjn112@naver.com>
2022-04-27 22:31:01 +0900

    Update 1476.cpp

--- BOJ/1476.cpp
+++ BOJ/1476.cpp
@@ -3,47 +3,31 @@ using namespace std ;
 
 int main()
 {
-	int e = 0 ;
-	int s = 0 ;
-	int m = 0 ;
+	int e ;
+	int s ;
+	int m ;
 
 	cin >> e >> s >> m ;
 
-	int iterE = 1 ;
-	int iterS = 1 ;
-	int iterM = 1 ;
+	e -= 1 ;
+	s -= 1 ;
+	m -= 1 ;
 
-	int year = 1 ;
+	int i = 0 ;
 
 	while ( true )
 	{
-		if ( iterE == e && iterS == s && iterM == m )
+		if ( e == i % 15 &&
+			 s == i % 28 &&
+			 m == i % 19 )
 		{
-			cout << year << '\n' ;
 			break ;
 		}
 
-		year++ ;
-
-		iterE++ ;
-		iterS++ ;
-		iterM++ ;
-
-		if ( iterE > 15 )
-		{
-			iterE = 1 ;
-		}
-
-		if ( iterS > 28 )
-		{
-			iterS = 1 ;
-		}
-
-		if ( iterM > 19 )
-		{
-			iterM = 1 ;
-		}
+		i++ ;
 	}
+
+	cout << i + 1 << "\n" ;
 
 	return 0 ;
 }
